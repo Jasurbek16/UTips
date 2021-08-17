3be6d467ba21e9054c3d841eabef282631181d99
Jasurbek Mamurov <jascinmamurov7@gmail.com>
2021-08-17 09:19:00 +0500

    updated

--- UTips/settings.py
+++ UTips/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
