2c4e9fd64f2ccff30f4053062a0171d889fe449c
Jasurbek Mamurov <jascinmamurov7@gmail.com>
2021-08-17 14:07:04 +0500

    update2

--- UTipsApp/urls.py
+++ UTipsApp/urls.py
@@ -1,5 +1,6 @@
 """Defines URL patterns for UniTipsApp"""
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import TopicEditView, TopicDeleteView
 from . import views
 
@@ -8,10 +9,14 @@ urlpatterns = [
     path("", views.home, name="Ut-home"),
     path("user/<str:name>", views.UserTopics, name="user-topics"),
     path("about/", views.about, name="Ut-about"),
-    path("subjects/<int:pk>/", views.subject_details, name="subject-details"),
+    
     path("topics/<int:pk>/", views.topic_details, name="topic-details"),
     path("topics/<int:pk>/edit/", TopicEditView.as_view(), name="edit-topic"),
     path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="delete-topic"),
+    
+    path("subjects/<int:pk>/", views.subject_details, name="subject-details"),
     path("subjects/new/", views.addSubject, name="add-subject"),
     path("subjects/<int:pk>/topics/new/", views.addTopic, name="add-topic"),
+
+
 ]
