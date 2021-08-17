2bd5961798591d8e4f04eea8b0871a6d8d860dee
Jasurbek Mamurov <jascinmamurov7@gmail.com>
2021-08-17 14:01:35 +0500

    keys

--- UTips/settings.py
+++ UTips/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "e46749b6def1f2cdefe4b2aad41465605121ffbb07bb4e26"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
