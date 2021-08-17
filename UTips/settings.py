43404fe30f68fecfd0745d868ec3c2e66b5af9d0
Jasurbek Mamurov <jascinmamurov7@gmail.com>
2021-08-17 21:20:30 +0500

    set debug_value

--- UTips/settings.py
+++ UTips/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['utipss.herokuapp.com', '127.0.0.1']
 
