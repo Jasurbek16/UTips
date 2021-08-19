fbfcb92139084622905ef2f8b8ea940dd81b0c55
Jasurbek Mamurov <jascinmamurov7@gmail.com>
2021-08-19 17:22:57 +0500

    add

--- UTips/settings.py
+++ UTips/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 
